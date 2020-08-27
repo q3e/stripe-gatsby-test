50f3f66f9aae764edff52764ef65eaee948f433d
fata <nfatah41@gmail.com>
2020-08-27 22:02:15 +0300

    add checkout functionality

--- src/sections/parts/Pricing.js
+++ src/sections/parts/Pricing.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { Container, Row, Col } from "react-bootstrap";
-import { rgba } from "polished";
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Container, Row, Col, Spinner } from 'react-bootstrap'
+import { rgba } from 'polished'
+import { loadStripe } from '@stripe/stripe-js'
 
 import {
   Title,
@@ -11,13 +12,13 @@ import {
   Badge,
   Button,
   Switch,
-} from "../../components/Core";
-import { device } from "../../utils";
+} from '../../components/Core'
+import { device } from '../../utils'
 
 const SectionStyled = styled(Section)`
   position: relative;
   &::after {
-    content: "";
+    content: '';
     left: 0;
     bottom: 0;
     height: 30%;
@@ -25,7 +26,7 @@ const SectionStyled = styled(Section)`
     position: absolute;
     background: ${({ theme }) => theme.colors.dark}!important;
   }
-`;
+`
 
 const ULStyled = styled.ul`
   list-style: none;
@@ -51,8 +52,8 @@ const ULStyled = styled.ul`
     margin-bottom: 5px;
 
     &:before {
-      content: "\f00c";
-      font-family: "Font Awesome 5 Free";
+      content: '\f00c';
+      font-family: 'Font Awesome 5 Free';
       font-weight: 900;
       display: inline-block;
       font-size: 13px;
@@ -69,7 +70,7 @@ const ULStyled = styled.ul`
       margin-right: 13px;
     }
   }
-`;
+`
 
 const CardPricing = styled(Box)`
   box-shadow: ${({ theme }) =>
@@ -82,7 +83,7 @@ const CardPricing = styled(Box)`
     width: 100% !important;
     border-radius: 0 0 10px 10px !important;
   }
-`;
+`
 
 const TitleSmall = styled.h4`
   color: ${({ theme }) => theme.colors.secondary};
@@ -90,7 +91,7 @@ const TitleSmall = styled.h4`
   font-weight: 300;
   letter-spacing: -0.5px;
   line-height: 28px;
-`;
+`
 
 const Currency = styled.span`
   color: ${({ theme }) => theme.colors.dark};
@@ -99,7 +100,7 @@ const Currency = styled.span`
   letter-spacing: -0.52px;
   line-height: 1;
   margin-bottom: 5px;
-`;
+`
 
 const TimePer = styled.span`
   color: ${({ theme }) => theme.colors.dark};
@@ -108,10 +109,38 @@ const TimePer = styled.span`
   letter-spacing: -0.52px;
   line-height: 1;
   margin-bottom: 5px;
-`;
+`
+
+let stripePromise
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
+  }
+  return stripePromise
+}
 
 const Pricing = () => {
-  const [timeMonthly, setTimeMonthly] = useState(false);
+  const [timeMonthly, setTimeMonthly] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const redirectToCheckout = (pricingTier) => async (event) => {
+    console.log(pricingTier)
+    event.preventDefault()
+    setLoading(true)
+
+    const stripe = await getStripe()
+    const { error } = await stripe.redirectToCheckout({
+      mode: 'subscription',
+      lineItems: [{ price: pricingTier, quantity: 1 }],
+      successUrl: `http://localhost:8000/payment-success/`,
+      cancelUrl: `http://localhost:8000/paymen-cancel`,
+    })
+
+    if (error) {
+      console.warn('Error:', error)
+      setLoading(false)
+    }
+  }
 
   return (
     <>
@@ -128,7 +157,7 @@ const Pricing = () => {
               <div className=" text-center">
                 <Title>Pricing &amp; Plans</Title>
                 <Text>
-                  Create custom landing pages with Omega that converts{" "}
+                  Create custom landing pages with Omega that converts{' '}
                   <br className="d-none d-md-block" /> more visitors than any
                   website.
                 </Text>
@@ -173,7 +202,18 @@ const Pricing = () => {
                       <li>Lifetime updates</li>
                     </ULStyled>
                   </div>
-                  <Button bg="secondary">Start 14 Days Free Trial</Button>
+                  <Button
+                    bg="secondary"
+                    onClick={redirectToCheckout(
+                      `${
+                        timeMonthly
+                          ? process.env.GATSBY_INDIVIDUAL_MONTHLY_PRICE_ID
+                          : process.env.GATSBY_TEAM_MONTHLY_PRICE_ID
+                      }`,
+                    )}
+                  >
+                    Start 14 Days Free Trial
+                  </Button>
                 </CardPricing>
               </Col>
               <Col lg="6" className="mb-5">
@@ -202,7 +242,18 @@ const Pricing = () => {
                       <li>Lifetime updates</li>
                     </ULStyled>
                   </div>
-                  <Button bg="secondary">Start 14 Days Free Trial</Button>
+                  <Button
+                    bg="secondary"
+                    onClick={redirectToCheckout(
+                      `${
+                        timeMonthly
+                          ? process.env.GATSBY_INDIVIDUAL_YEARLY_PRICE_ID
+                          : process.env.GATSBY_TEAM_YEARLY_PRICE_ID
+                      }`,
+                    )}
+                  >
+                    Start 14 Days Free Trial
+                  </Button>
                 </CardPricing>
               </Col>
             </Row>
@@ -210,7 +261,7 @@ const Pricing = () => {
         </Container>
       </SectionStyled>
     </>
-  );
-};
+  )
+}
 
-export default Pricing;
+export default Pricing
